3280e2450b7e6775789cb248ea3704146961fbd9
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 01:15:58 +0600

    feat: added SectionHeading component

--- src/layouts/PremiumSection/PremiumSection.jsx
+++ src/layouts/PremiumSection/PremiumSection.jsx
@@ -1,7 +1,13 @@
+import SectionHeading from '../../components/SectionHeading/SectionHeading.jsx';
+
 const PremiumSection = () => {
     return (
         <>
-        
+            <div className="w-full py-10 md:py-20 lg:py-[120px]">
+                <div className="container">
+                    <SectionHeading title={'Premium Digital Tools'} paragraph={'Choose from our curated collection of premium digital products designed to boost your productivity and creativity.'}></SectionHeading>
+                </div>
+            </div>
         </>
     );
 };
