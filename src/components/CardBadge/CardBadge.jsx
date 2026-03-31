37cd2e5bd85a5245cee5edfd394cc541ed9098da
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 02:53:51 +0600

    feat: implement reuseable card badge component

--- src/components/CardBadge/CardBadge.jsx
+++ src/components/CardBadge/CardBadge.jsx
@@ -1,7 +1,17 @@
-const CardBadge = () => {
+const CardBadge = ({ children, variant = 'New'}) => {
+    const defaultStyle = "font-family text-xs font-medium md:text-sm";
+
+    const style = {
+        "Best Seller": "text-[#BB4D00] bg-[#FEF3C6] rounded-full px-2 py-1 md:px-2.5 md:py-[5px] lg:px-3 lg:py-[6px]",
+        "New": "text-[#0A883E] bg-[#DBFCE7] rounded-full px-2 py-1 md:px-2.5 md:py-[5px] lg:px-3 lg:py-[6px]",
+        "Popular": "text-[#4F39F6] bg-[#E1E7FF] rounded-full px-2 py-1 md:px-2.5 md:py-[5px] lg:px-3 lg:py-[6px]"
+    };
+
     return (
         <>
-        
+            <span className={`${defaultStyle} ${style[variant]}`}>
+                {children}    
+            </span>
         </>
     );
 };
