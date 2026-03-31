68f46197e09ab88ba16ab388060e60f13128b783
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 01:07:49 +0600

    feat: added MainSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import HeaderSection from './layouts/HeaderSection/HeaderSection.jsx';
+import MainSection from './layouts/MainSection/MainSection.jsx';
 
 const App = () => {
     return (
         <>
             <div className="relative overflow-hidden">
                 <HeaderSection></HeaderSection>
+                <MainSection></MainSection>
             </div>
         </>
     );
