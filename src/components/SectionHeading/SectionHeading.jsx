cea5405c65967414e91728791e727708ed647cde
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 01:26:15 +0600

    feat: implement section heading text and paragraph

--- src/components/SectionHeading/SectionHeading.jsx
+++ src/components/SectionHeading/SectionHeading.jsx
@@ -1,7 +1,10 @@
-const SectionHeading = () => {
+const SectionHeading = ({ title, paragraph }) => {
     return (
         <>
-        
+            <div className="w-full flex items-center justify-center flex-col text-center gap-4">
+                <h1 className="font-family text-[32px] font-extrabold text-[#101727] md:text-[40px] lg:text-[48px]">{title}</h1>
+                <p className="w-full max-w-[550px] font-family text-sm font-normal text-[#627382] md:text-[15px] lg:text-base">{paragraph}</p>
+            </div>
         </>
     );
 };
