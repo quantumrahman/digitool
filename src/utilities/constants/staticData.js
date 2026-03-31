cbed47a0a961166f7b6e0883ebf1172a11a39afa
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-03-31 20:52:35 +0600

    add: steps data array of object and export it

---
+++ src/utilities/constants/staticData.js
@@ -0,0 +1,23 @@
+export const stepsData = [
+    {
+        id: 1,
+        stepNumber: "01",
+        title: "Create Account",
+        description: "Sign up for free in seconds. No credit card required to get started.",
+        icon: "user"
+    },
+    {
+        id: 2,
+        stepNumber: "02",
+        title: "Choose Products",
+        description: "Browse our catalog and select the tools that fit your needs.",
+        icon: "box"
+    },
+    {
+        id: 3,
+        stepNumber: "03",
+        title: "Start Creating",
+        description: "Download and start using your premium tools immediately.",
+        icon: "rocket"
+    }
+];
