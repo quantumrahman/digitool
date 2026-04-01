e6492f5a4f60938e6dff0eebb1f887b5f732c853
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 10:10:36 +0600

    feat: show cart info in display

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,14 +1,14 @@
-const CartCard = () => {
+const CartCard = ({ product }) => {
     return (
         <>
             <div className="w-full flex items-center justify-between p-4 bg-[#F9FAFC] rounded-2xl md:p-[20px]">
                 <div className="w-auto flex items-center justify-center gap-4">
-                    <div className="w-[60px] h-[60px] bg-white border border-[#F2F2F2] rounded-full flex items-center justify-center">
-                        <img src="" alt="" />
+                    <div className="w-[60px] h-[60px] bg-white border border-[#F2F2F2] rounded-full flex items-center justify-center overflow-hidden">
+                        <img src={product?.icon} alt="product icon" />
                     </div>
                     <div className="w-auto space-y-2">
-                        <h4 className="font-family text-base font-semibold text-[#101727] md:text-lg lg:text-xl">AI Writing Pro</h4>
-                        <p className="font-family text-sm font-medium text-[#627382] md:text-base">$20</p>
+                        <h4 className="font-family text-base font-semibold text-[#101727] md:text-lg lg:text-xl">{product?.name}</h4>
+                        <p className="font-family text-sm font-medium text-[#627382] md:text-base">${product?.price}</p>
                     </div>
                 </div>
                 <button className="font-family text-sm font-bold text-[#FF3980] cursor-pointer transition-all duration-300 delay-300 hover:underline md:text-base">Remove</button>
