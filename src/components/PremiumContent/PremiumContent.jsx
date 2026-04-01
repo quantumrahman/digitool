c9444d98be97b24b29741dfe6f22274427d8e7a2
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 07:34:51 +0600

    feat: data mapping and display in ui

--- src/components/PremiumContent/PremiumContent.jsx
+++ src/components/PremiumContent/PremiumContent.jsx
@@ -4,14 +4,12 @@ import ProductCard from '../../components/ProductCard/ProductCard.jsx';
 const PremiumContent = ({ productPromies }) => {
     const products = use(productPromies);
 
-    console.log(products);
-
     return (
         <>
             <div className='w-full mt-5 grid grid-cols-1 gap-3 sm:gap-4 md:mt-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:gap-[30px]'>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
-                <ProductCard></ProductCard>
+                {products?.products.map((product) => (
+                    <ProductCard key={product.id} product={product}></ProductCard>
+                ))}
             </div>
         </>
     );
