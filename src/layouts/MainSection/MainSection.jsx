718e67f2aba3f2dec19ac3b2e51403655a2eda8c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 17:49:58 +0600

    add: StatsSection.jsx component added

--- src/layouts/MainSection/MainSection.jsx
+++ src/layouts/MainSection/MainSection.jsx
@@ -3,12 +3,14 @@ import PremiumSection from '../PremiumSection/PremiumSection.jsx';
 import PricingSection from '../PricingSection/PricingSection.jsx';
 import StepsSection from '../StepsSection/StepsSection.jsx';
 import LetterSection from '../LetterSection/LetterSection.jsx';
+import StatsSection from '../StatsSection/StatsSection.jsx';
 
 const MainSection = () => {
     const productPromies = fetchProduct();
     
     return (
         <>
+            <StatsSection></StatsSection>
             <PremiumSection productPromies={productPromies}></PremiumSection>
             <StepsSection></StepsSection>
             <PricingSection></PricingSection>
