a9e68a963d1d189fdc653ab4f017a92224cc8bab
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 18:31:25 +0600

    add: added HomeSection component

--- src/layouts/MainSection/MainSection.jsx
+++ src/layouts/MainSection/MainSection.jsx
@@ -1,15 +1,17 @@
 import fetchProduct from '../../api/productApi.js';
-import PremiumSection from '../PremiumSection/PremiumSection.jsx';
-import PricingSection from '../PricingSection/PricingSection.jsx';
-import StepsSection from '../StepsSection/StepsSection.jsx';
-import LetterSection from '../LetterSection/LetterSection.jsx';
+import HomeSection from '../HomeSection/HomeSection.jsx';
 import StatsSection from '../StatsSection/StatsSection.jsx';
+import PremiumSection from '../PremiumSection/PremiumSection.jsx';
+import StepsSection from '../StepsSection/StepsSection.jsx';
+import PricingSection from '../PricingSection/PricingSection.jsx';
+import LetterSection from '../LetterSection/LetterSection.jsx';
 
 const MainSection = () => {
     const productPromies = fetchProduct();
     
     return (
         <>
+            <HomeSection></HomeSection>
             <StatsSection></StatsSection>
             <PremiumSection productPromies={productPromies}></PremiumSection>
             <StepsSection></StepsSection>
