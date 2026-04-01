8d0718e7fdf7f2048a3de52488080b0ab30ea0a9
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 13:23:38 +0600

    feat: implement StepCard component

--- src/layouts/StepsSection/StepsSection.jsx
+++ src/layouts/StepsSection/StepsSection.jsx
@@ -1,3 +1,4 @@
+import { stepsData } from "../../utilities/constants/staticData.js";
 import SectionHeading from "../../components/SectionHeading/SectionHeading.jsx";
 import StepCard from "../../components/StepCard/StepCard.jsx";
 
@@ -8,7 +9,9 @@ const StepsSection = () => {
                 <div className="container">
                     <SectionHeading title={'Get Started in 3 Steps'} paragraph={'Start using premium digital tools in minutes, not hours.'}></SectionHeading>
                     <div className="w-full mt-5 grid grid-cols-1 gap-3 sm:gap-4 md:mt-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:gap-[30px]">
-                        <StepCard></StepCard>
+                        {stepsData.map((step) => (
+                            <StepCard key={step.id} id={step.id} title={step.title} paragraph={step.description} image={step.icon}></StepCard>
+                        ))}
                     </div>
                 </div>
             </div>
