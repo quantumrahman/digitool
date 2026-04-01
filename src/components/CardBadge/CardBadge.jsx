80d3ca1e50fd3d470ae7fad1d30aa29f64b87b34
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 19:13:22 +0600

    modify: CardBadge and PricingCard

--- src/components/CardBadge/CardBadge.jsx
+++ src/components/CardBadge/CardBadge.jsx
@@ -3,6 +3,7 @@ const CardBadge = ({ children, variant = 'New'}) => {
 
     const style = {
         "Best Seller": "text-[#BB4D00] bg-[#FEF3C6] rounded-full px-2 py-1 md:px-2.5 md:py-[5px] lg:px-3 lg:py-[6px]",
+        "Most Popular": "text-[#BB4D00] bg-[#FEF3C6] rounded-full px-2 py-1 md:px-2.5 md:py-[5px] lg:px-3 lg:py-[6px]",
         "New": "text-[#0A883E] bg-[#DBFCE7] rounded-full px-2 py-1 md:px-2.5 md:py-[5px] lg:px-3 lg:py-[6px]",
         "Popular": "text-[#4F39F6] bg-[#E1E7FF] rounded-full px-2 py-1 md:px-2.5 md:py-[5px] lg:px-3 lg:py-[6px]"
     };
--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -1,10 +1,16 @@
 import { Check } from "lucide-react";
+import CardBadge from "../CardBadge/CardBadge.jsx";
 
 const PricingCard = ({ title, tagline, price, duration, isPopular, buttonText, features }) => {
     
     return (
         <>
             <div className={`w-full relative p-4 ${isPopular ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA]' : 'bg-[#F9FAFC]'} border border-[#F2F2F2] rounded-2xl flex items-center flex-col gap-4 md:p-5 md:gap-6 lg:p-6`}>
+                {isPopular && 
+                    <div className="w-full absolute -top-4 left-0 flex items-center justify-center">
+                        <CardBadge variant="Most Popular">Most Popular</CardBadge>
+                    </div>
+                }
                 <div className="w-full">
                     <h4 className={`font-family text-lg font-bold ${isPopular ? 'text-white' : 'text-[#101727]'} md:text-xl lg:text-2xl`}>{title}</h4>
                     <p className={`font-family text-sm font-normal ${isPopular ? 'text-white/80' : 'text-[#101727]'} mt-2 md:text-base`}>{tagline}</p>
