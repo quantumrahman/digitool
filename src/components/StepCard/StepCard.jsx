3b8d6a9c191e973d8ef30bdcddebc44e1f4e7b1b
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 13:17:00 +0600

    feat: implement StepsCard component design

--- src/components/StepCard/StepCard.jsx
+++ src/components/StepCard/StepCard.jsx
@@ -1,7 +1,27 @@
-const StepCard = () => {
+import User from '../../assets/icons/user.png';
+import Box from '../../assets/icons/package.png';
+import Rocket from '../../assets/icons/rocket.png';
+
+const StepCard = ({ id, title, paragraph, image }) => {
+
     return (
         <>
-        
+            <div className="w-full min-h-[380px] relative bg-white border border-[#F1F1F1] rounded-2xl flex items-center justify-center flex-col gap-[14px] p-4 md:p-5 md:gap-7 lg:p-6">
+                <div className="w-full flex items-center justify-end absolute top-6 right-6">
+                    <div className="w-10 h-10 bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full flex items-center justify-center">
+                        <span className="font-family text-xs font-bold text-white md:text-sm">0{id}</span>
+                    </div>
+                </div>
+                <div className="w-full flex items-center justify-center flex-col gap-4">
+                    <div className="w-[100px] h-[100px] bg-linear-to-r from-[#4F39F6]/20 to-[#9514FA]/20 rounded-full flex items-center justify-center">
+                        {image === 'user' && <img src={User} alt={`${image} icon`} />}
+                        {image === 'box' && <img src={Box} alt={`${image} icon`} />}
+                        {image === 'rocket' && <img src={Rocket} alt={`${image} icon`} />}
+                    </div>
+                    <h4 className="font-family text-lg font-bold text-[#101727] md:text-xl lg:text-2xl">{title}</h4>
+                    <p className="font-family text-sm font-normal text-[#627382] md:text-base text-center">{paragraph}</p>
+                </div>
+            </div>
         </>
     );
 };
--- src/layouts/StepsSection/StepsSection.jsx
+++ src/layouts/StepsSection/StepsSection.jsx
@@ -1,7 +1,17 @@
+import SectionHeading from "../../components/SectionHeading/SectionHeading.jsx";
+import StepCard from "../../components/StepCard/StepCard.jsx";
+
 const StepsSection = () => {
     return (
         <>
-        
+            <div className="w-full py-10 bg-[#F9FAFC] md:py-20 lg:py-[120px]">
+                <div className="container">
+                    <SectionHeading title={'Get Started in 3 Steps'} paragraph={'Start using premium digital tools in minutes, not hours.'}></SectionHeading>
+                    <div className="w-full mt-5 grid grid-cols-1 gap-3 sm:gap-4 md:mt-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:gap-[30px]">
+                        <StepCard></StepCard>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
