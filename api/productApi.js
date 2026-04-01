ae846b6a9cb4c857ed0911290e03a1f403ba6812
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 08:18:30 +0600

    fix: some issue

--- api/productApi.js
+++ api/productApi.js
@@ -1,6 +1,6 @@
 const fetchProduct = async () => {
     try {
-        const response = await fetch('./public/product.json');
+        const response = await fetch('../public/product.json');
         return await response.json();
     } catch (error) {
         console.log(error);
