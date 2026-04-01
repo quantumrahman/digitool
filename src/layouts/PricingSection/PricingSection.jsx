918479c57bcbd837d0dc28bb3ccd5f12f2bb5439
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 13:29:51 +0600

    feat: implement PricingCard component

--- src/layouts/PricingSection/PricingSection.jsx
+++ src/layouts/PricingSection/PricingSection.jsx
@@ -1,3 +1,4 @@
+import PricingCard from "../../components/PricingCard/PricingCard";
 import SectionHeading from "../../components/SectionHeading/SectionHeading";
 
 const PricingSection = () => {
@@ -7,7 +8,7 @@ const PricingSection = () => {
                 <div className="container">
                     <SectionHeading title={'Simple, Transparent Pricing'} paragraph={'Choose the plan that fits your needs. Upgrade or downgrade anytime.'}></SectionHeading>
                     <div className="w-full mt-5 grid grid-cols-1 gap-3 sm:gap-4 md:mt-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:gap-[30px]">
-                        
+                        <PricingCard></PricingCard>
                     </div>
                 </div>
             </div>
