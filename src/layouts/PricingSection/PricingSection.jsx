0ace31bee192bd292cfbcee89e514b14afa6613b
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-01 13:28:42 +0600

    feat: implement SectionHeading component

--- src/layouts/PricingSection/PricingSection.jsx
+++ src/layouts/PricingSection/PricingSection.jsx
@@ -1,7 +1,16 @@
+import SectionHeading from "../../components/SectionHeading/SectionHeading";
+
 const PricingSection = () => {
     return (
         <>
-        
+            <div className="w-full py-10 md:py-20 lg:py-[120px]">
+                <div className="container">
+                    <SectionHeading title={'Simple, Transparent Pricing'} paragraph={'Choose the plan that fits your needs. Upgrade or downgrade anytime.'}></SectionHeading>
+                    <div className="w-full mt-5 grid grid-cols-1 gap-3 sm:gap-4 md:mt-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:gap-[30px]">
+                        
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
